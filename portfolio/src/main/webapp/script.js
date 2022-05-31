e60944dedef039bf4f201fe1dc784b8d9e641806
Blubaeri <evanlin2912@gmail.com>
2022-05-31 14:50:24 +0000

    Change background color, add image on index.html, add contact and about page, add a nav bar, change the random message

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -17,7 +17,7 @@
  */
 function addRandomGreeting() {
   const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+      ['a software engineer', 'an artist', 'chinese', 'from New York'];
 
   // Pick a random greeting.
   const greeting = greetings[Math.floor(Math.random() * greetings.length)];
